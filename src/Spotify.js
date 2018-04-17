d0555a67ca53a4509750b40f76552be0103a7c99
Gabriel Cunha <gabriel_mcunha@hotmail.com>
2018-04-17 17:42:39 -0300

    Add Album Tracks Component

--- src/Spotify.js
+++ src/Spotify.js
@@ -1,7 +1,7 @@
 import SpotifyWrapper from 'spotify-wrapper';
 
 const spotify = new SpotifyWrapper({
-	token: 'BQBowXkMpQX2LMwjn7bO1wnJHXpJGzc8z5P2cMEKUlckLnLxN2aeG_LKnnpUtjsfcv-23-gS1BY_XDhzZ36NhDGZzjtftz1fJCSxaU2XYn-NqazIxgN4W-WQ3q9rvXd3nqRY-jkP1aU4OxzImGlB2PF7q_D6x-w'
+	token: 'BQB-4oa0eSaOGDYG1XI_pbcQNy_bbgY-RNnYr8nO52rcfK0n536c3mCLL-t2GBdYq8szUCfqyjYvAvduFcMaP07iUc2sQvVhlg4rwE6BFtZw6Qj2fsui78n76BGSLeLGNBHes6urs7_RHnkehJLvYsMFKsee_ZI'
 });
 
 export default spotify;
