26ede0d971a07a57ae5fae8538f0e6d1e1e69713
Gabriel Cunha <gabriel_mcunha@hotmail.com>
2018-04-17 09:32:51 -0300

    Add Album Info Component

--- src/Spotify.js
+++ src/Spotify.js
@@ -1,7 +1,7 @@
 import SpotifyWrapper from 'spotify-wrapper';
 
 const spotify = new SpotifyWrapper({
-	token: 'YOUR_TOKEN_HERE'
+	token: 'BQBowXkMpQX2LMwjn7bO1wnJHXpJGzc8z5P2cMEKUlckLnLxN2aeG_LKnnpUtjsfcv-23-gS1BY_XDhzZ36NhDGZzjtftz1fJCSxaU2XYn-NqazIxgN4W-WQ3q9rvXd3nqRY-jkP1aU4OxzImGlB2PF7q_D6x-w'
 });
 
 export default spotify;
